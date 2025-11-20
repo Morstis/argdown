82a9089c2536b2891c71c9a60ca76f541a338fba
Lucas Wiese <lucas.wiese@gmx.de>
2025-11-20 13:40:36 +0100

    remove nyc, babel and jest
    remove babel because building is done with tsc now and I can not find
    any working babel config. remove jest because e2e tests are done with
    mocha. remove nyc because code coverage for e2e tests don't seem to make
    sense. also no working config for this anywayb, because mocha executes
    on dist *js files and nyc would test agains *ts files.

--- packages/argdown-cli/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testMatch: ["**/test/**/*.spec.ts"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
