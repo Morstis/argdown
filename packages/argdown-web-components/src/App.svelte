fd8c66d326e3562f554553f7fa399de104ff85a8
Lucas Wiese <lucas.wiese@gmx.de>
2026-02-05 20:41:52 +0100

    init svelte web component library

---
+++ packages/argdown-web-components/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  let name = 'world';
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Welcome to your new Svelte app. Feel free to explore and customize it!</p>
+  <argdown-counters></argdown-counters>
+</main>
---
+++ packages/argdown-web-components/src/main.ts
@@ -0,0 +1,2 @@
+import "./lib/Counter.svelte";
+import "./lib/Counter2.svelte";
