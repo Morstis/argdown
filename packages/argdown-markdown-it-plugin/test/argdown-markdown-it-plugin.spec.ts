11c48503fa6826a9e447acda791babfbdb987e84
Lucas Wiese <lucas.wiese@gmx.de>
2026-02-09 12:03:21 +0100

    [markdown-it] testcase describe should be sync and async code should be executed in before
    Signed-off-by: Lucas Wiese <lucas.wiese@gmx.de>

--- packages/argdown-markdown-it-plugin/test/argdown-markdown-it-plugin.spec.ts
+++ packages/argdown-markdown-it-plugin/test/argdown-markdown-it-plugin.spec.ts
@@ -3,9 +3,11 @@ import { describe, it } from "mocha";
 import MarkdownIt from "markdown-it";
 import createArgdownPlugin from "../src/argdown-markdown-it-plugin";
 
-describe("Markdown It! Argdown Plugin", async function () {
+describe("Markdown It! Argdown Plugin", function () {
   const mdi = new MarkdownIt();
-  mdi.use(await createArgdownPlugin());
+  before(async function () {
+    mdi.use(await createArgdownPlugin());
+  });
   this.timeout(5000);
   it("can replace code fences with argument maps", function () {
     const result = mdi.render(`
